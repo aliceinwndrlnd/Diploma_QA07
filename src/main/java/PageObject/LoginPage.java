d2e2164abcf33944574583453f7a198af81dad2f
aliceinwndrlnd <yurkevich.alesya@gmail.com>
2021-10-31 17:29:57 +0300

    LoginPage locators

--- src/main/java/PageObject/LoginPage.java
+++ src/main/java/PageObject/LoginPage.java
@@ -1,5 +1,22 @@
 package PageObject;
 
+import com.codeborne.selenide.SelenideElement;
+import org.openqa.selenium.support.FindBy;
+
 public class LoginPage {
+
+    @FindBy (className = "kf-HTgR-ee339")
+    SelenideElement enterButton;
+
+    @FindBy (id = "email")
+    SelenideElement inputEmail;
+
+    @FindBy (id = "password")
+    SelenideElement inputPassword;
+
+    @FindBy (className = "kf-cS-8bbc7 kf-cLS-c2977 kf-cLA-d9cc7 kf-cLZ-3e5d1")
+    SelenideElement submitButton;
+
+
 
 }
