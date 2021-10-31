ea0b7d384af7299b0b35859984489575df012bd6
aliceinwndrlnd <yurkevich.alesya@gmail.com>
2021-10-31 18:02:31 +0300

    PageObject LoginPage

--- src/main/java/PageObject/LoginPage.java
+++ src/main/java/PageObject/LoginPage.java
@@ -2,6 +2,7 @@ package PageObject;
 
 import com.codeborne.selenide.SelenideElement;
 import org.openqa.selenium.support.FindBy;
+import org.testng.Assert;
 
 public class LoginPage {
 
@@ -17,13 +18,19 @@ public class LoginPage {
     @FindBy (className = "kf-cS-8bbc7 kf-cLS-c2977 kf-cLA-d9cc7 kf-cLZ-3e5d1")
     SelenideElement submitButton;
 
-    public LoginPage enterEmail() {
-        inputEmail.setValue("AutomationTestQA@yahoo.com");
+    @FindBy (className = "kf-HbkZ-b0d48")
+    SelenideElement icons;
+
+    @FindBy (className = "kf-HRkA-a1267")
+    SelenideElement text;
+
+    public LoginPage enterEmail(String email) {
+        inputEmail.setValue(email);
         return this;
     }
 
-    public LoginPage enterPassword() {
-        inputPassword.setValue("AutomationTestQA1234");
+    public LoginPage enterPassword(String password) {
+        inputPassword.setValue(password);
         return this;
     }
 
@@ -32,6 +39,13 @@ public class LoginPage {
         return this;
     }
 
+    public LoginPage verifyIcons() {
+        icons.isEnabled();
+        return this;
+    }
 
-
+    public LoginPage checkingText() {
+        Assert.assertEquals(text.getText(), "Войдите через сервисы:");
+        return this;
+    }
 }
