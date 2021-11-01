5533629fcc3bb10251c65dd593ca6d4252a2dd70
aliceinwndrlnd <yurkevich.alesya@gmail.com>
2021-11-01 16:41:00 +0300

    driver+utils

--- src/main/java/Driver/BaseTestSelenide.java
+++ src/main/java/Driver/BaseTestSelenide.java
@@ -2,17 +2,19 @@ package Driver;
 
 import Properties.PropertyReader;
 import TestNG.Listener;
+import lombok.extern.log4j.Log4j2;
 import org.testng.annotations.Listeners;
+
 
 import static com.codeborne.selenide.Selenide.open;
 import static com.codeborne.selenide.Selenide.page;
 import static com.codeborne.selenide.WebDriverRunner.driver;
 
+@Log4j2
 @Listeners(Listener.class)
 public class BaseTestSelenide {
 
     public <PageObjectClass> PageObjectClass get(Class<PageObjectClass> pageObjectClassClass) {
         return driver().hasWebDriverStarted() ? page(pageObjectClassClass) : open(PropertyReader.getProperties().getProperty("url"), pageObjectClassClass);
     }
-
 }
