b694571f54c717dd1545a9a5c2b870d314972a2d
aliceinwndrlnd <yurkevich.alesya@gmail.com>
2021-11-03 18:46:16 +0300

    RegistrationDataPage + negativeTest

--- src/test/java/NegativeTests_Kufar.java
+++ src/test/java/NegativeTests_Kufar.java
@@ -1,6 +1,7 @@
 import Driver.BaseTestSelenide;
 import PageObject.HomePage;
 import PageObject.LoginPage;
+import PageObject.RegistrationDataPage;
 import PageObject.SettingsPage;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.Test;
@@ -37,6 +38,28 @@ public class NegativeTests_Kufar extends BaseTestSelenide {
                 .enterName("илооллллллллллллллисвылотсатцуолаиииииииииииииииииииииииииииасилуцотвлуцо")
                 .clickSave()
                 .checkingValidationOfName();
+    }
+
+    @Test
+    public void registrationDataPageTest() {//тест на ввод данных, не соответвующих минимальному количеству символов
+        get(HomePage.class)
+                .сlosePopUp();
+        get(LoginPage.class)
+                .clickLoginPage()
+                .enterEmail("AutomationTestQA@yahoo.com")
+                .enterPassword("AutomationTestQA1234")
+                .clickSubmit();
+        get(SettingsPage.class)
+                .clickOnPfofile()
+                .clickSettings();
+        get(RegistrationDataPage.class)
+                .registrationDataPageClick()
+                .changePasswordButtonClick()
+                .verifyTitle()
+                .enterCurrentPassword("1")
+                .enterPassword("1")
+                .enterConfirmPassword(" ")
+                .checkErrorText();
     }
 
 
