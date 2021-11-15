082d23c914962f126d9f9d5c74fe41b5af59ef3b
aliceinwndrlnd <yurkevich.alesya@gmail.com>
2021-11-15 16:51:57 +0300

    Api test

--- src/test/java/Rest_Api/APiTests.java
+++ src/test/java/Rest_Api/APiTests.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import static io.restassured.RestAssured.baseURI;
@@ -12,21 +13,39 @@ import static io.restassured.RestAssured.*;
 
 public class APiTests {
     String token;
+    String errorText;
     Gson gson = new Gson();
     ObjectMapper mapper = new ObjectMapper();
-    Login login = new Login() {{
+    Login correctLogin = new Login() {{
         setEmail("AutomationTestQA@yahoo.com");
         setPassword("AutomationTestQA1234");
+    }};
+    Login uncorrectLogin = new Login() {{
+        setEmail("AutomationTestQA@yahoo.com");
+        setPassword("ttt");
     }};
 
 
     @Test
-    public void authorization() throws JsonProcessingException {
+    public void successfulAuthorization() throws JsonProcessingException {
         baseURI = "https://www.kufar.by/";
-        String response = given().when().body(mapper.writeValueAsString(login))
+        String endpoint = "l/api/login/v2/auth/signin?token_type=user";
+        String response = given().when().body(mapper.writeValueAsString(correctLogin))
                 .and().header("Content-Type", "application/json").when()
-                .post("l/api/login/v2/auth/signin?token_type=user").getBody().asPrettyString();
+                .post(endpoint).getBody().asPrettyString();
         JsonObject jsonObject = gson.fromJson(response, JsonObject.class);
         token = jsonObject.get("jwt").getAsString();
+    }
+
+    @Test
+    public void unsuccessfulAuthorization() throws JsonProcessingException {
+        baseURI = "https://www.kufar.by/";
+        String endpoint = "l/api/login/v2/auth/signin?token_type=user";
+        String response = given().when().body(mapper.writeValueAsString(uncorrectLogin))
+                .and().header("Content-Type", "application/json").when()
+                .post(endpoint).getBody().asPrettyString();
+        JsonObject jsonObject = gson.fromJson(response, JsonObject.class);
+        errorText = jsonObject.get("message").getAsString();
+        Assert.assertEquals(errorText,"authentication failed");
     }
 }
