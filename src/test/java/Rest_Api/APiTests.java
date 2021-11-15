23436f53a4f336d8f5e16eaa0fe6786f7f9411d2
aliceinwndrlnd <yurkevich.alesya@gmail.com>
2021-11-15 12:23:05 +0300

    Api test

--- src/test/java/Rest_Api/APiTests.java
+++ src/test/java/Rest_Api/APiTests.java
@@ -22,10 +22,10 @@ public class APiTests {
 
     @BeforeTest
     public void preconditions() throws JsonProcessingException {
-        baseURI = "https://kufar.by/";
+        baseURI = "https://www.kufar.by/";
         String response = given().when().body(mapper.writeValueAsString(login))
                 .and().header("Content-Type", "application/json").when().post("l/api/login/v2/auth/signin?token_type=user").getBody().asPrettyString();
         JsonObject jsonObject = gson.fromJson(response, JsonObject.class);
-        token = jsonObject.get("token").getAsString();
+        token = jsonObject.get("jwt").getAsString();
     }
 }
