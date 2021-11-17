36770df933dd67dfd2cce34fe2908e3ff9cd23d5
aliceinwndrlnd <yurkevich.alesya@gmail.com>
2021-11-17 18:49:27 +0300

    BaseTestSelenide add AfterTest

--- src/main/java/Driver/BaseTestSelenide.java
+++ src/main/java/Driver/BaseTestSelenide.java
@@ -3,11 +3,11 @@ package Driver;
 import Properties.PropertyReader;
 import TestNG.Listener;
 import lombok.extern.log4j.Log4j2;
+import org.testng.annotations.AfterTest;
 import org.testng.annotations.Listeners;
 
 
-import static com.codeborne.selenide.Selenide.open;
-import static com.codeborne.selenide.Selenide.page;
+import static com.codeborne.selenide.Selenide.*;
 import static com.codeborne.selenide.WebDriverRunner.driver;
 
 @Log4j2
@@ -16,5 +16,10 @@ public class BaseTestSelenide {
 
     public <PageObjectClass> PageObjectClass get(Class<PageObjectClass> pageObjectClassClass) {
         return driver().hasWebDriverStarted() ? page(pageObjectClassClass) : open(PropertyReader.getProperties().getProperty("url"), pageObjectClassClass);
+    }
+
+    @AfterTest(alwaysRun = true)
+    public void close() {
+        closeWebDriver();
     }
 }
